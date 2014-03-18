f8bf8fa7c565e7f694051e9b9f73f51b1c70c4cf
Thomas <your.email@gmail.com>
2014-03-18 16:18:08 +0100

    updated comments

--- 4-flappy_bird/flappy_bird_final/menu.js
+++ 4-flappy_bird/flappy_bird_final/menu.js
@@ -1,17 +1,20 @@
 var menu_state = {
     create: function() {
-        // Call the 'play' function when pressing the spacebar
+        // Call the 'start' function when pressing the spacebar
         var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         space_key.onDown.add(this.start, this); 
 
-        // Adding a label centered on the screen
+        // Defining variables
         var style = { font: "30px Arial", fill: "#ffffff" };
         var x = game.world.width/2, y = game.world.height/2;
+        
+        // Adding a text centered on the screen
         var text = this.game.add.text(x, y-50, "Press space to start", style);
         text.anchor.setTo(0.5, 0.5); 
 
-        // If the user already played, display its previous score
+        // If the user already played
         if (score > 0) {
+            // Display its score
             var score_label = this.game.add.text(x, y+50, "score: " + score, style);
             score_label.anchor.setTo(0.5, 0.5); 
         }
